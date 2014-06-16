e979e623a50bcf76588facb7b04df05ad48c2488
Rodolfo Campos <camposer@gmail.com>
2014-06-16 12:39:16 +0200

    Actualizado ejemplo de Signature. Incluye verficación

--- crypto/src/crypto/signature/Generador.java
+++ crypto/src/crypto/signature/Generador.java
@@ -5,6 +5,8 @@ import java.nio.file.Files;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.Signature;
+import java.util.Hashtable;
+import java.util.Map;
 
 import crypto.util.UtilCrypto;
 
@@ -14,6 +16,8 @@ public class Generador {
 				new File("tmp/quijote.txt"),
 				new File("tmp/loremipsum.txt"),
 			};
+		Map<File, byte[]> firmas = 
+				new Hashtable<File, byte[]>();
 
 		// 1.- Generar claves públicas y privadas
 		KeyPairGenerator kpg = KeyPairGenerator
@@ -27,11 +31,25 @@ public class Generador {
 			sig.initSign(kpair.getPrivate()); // Firmando!
 			sig.update(Files.readAllBytes(f.toPath()));
 			byte[] firma = sig.sign();
+			firmas.put(f, firma);
 			System.out.println("Firma de " + f + ": " +
 					UtilCrypto.toHex(firma));
 		}
 		
+		System.out.println();
 		
+		// 2. Verificando la firma
+		for (File f : ficheros) {
+			Signature sig = Signature.getInstance("SHA1withRSA");
+			sig.initVerify(kpair.getPublic()); // Verificando!
+			sig.update(Files.readAllBytes(f.toPath()));
+
+			boolean verificado = sig.verify(firmas.get(f));
+			if (verificado)
+				System.out.println(f + " ha sido verificado");
+			else
+				System.out.println(f + " no ha sido verificado");
+		}
 		
 	}
 }
